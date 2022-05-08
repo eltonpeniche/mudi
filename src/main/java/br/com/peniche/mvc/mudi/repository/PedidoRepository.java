29082db739a821f2f77638d7c8c91dd907a98b21
eltonpeniche <elton_nfs@hotmail.com>
2022-05-08 17:23:37 -0300

    updates

--- src/main/java/br/com/peniche/mvc/mudi/repository/PedidoRepository.java
+++ src/main/java/br/com/peniche/mvc/mudi/repository/PedidoRepository.java
@@ -1,11 +1,16 @@
 package br.com.peniche.mvc.mudi.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.peniche.mvc.mudi.model.Pedido;
+import br.com.peniche.mvc.mudi.model.StatusPedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+
+	List<Pedido> findByStatus(StatusPedido status);
 
 }
